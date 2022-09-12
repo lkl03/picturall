9455ba2fad1c071eaca502a7a253434471ba3398
Luca <lulago17@gmail.com>
2022-09-11 21:38:25 -0300

    search bar functionality and minor details

--- picturall_frontend/src/components/Feed.jsx
+++ picturall_frontend/src/components/Feed.jsx
@@ -31,7 +31,7 @@ const Feed = () => {
   
   if(loading) return <Spinner message="We are adding new ideas to your feed!" />
 
-  
+  if(!pins?.length) return <h2>No Pins posted yet!</h2>
   return (
     <div>
       {pins && <MasonryLayout pins={pins} />}
--- picturall_frontend/src/components/Navbar.jsx
+++ picturall_frontend/src/components/Navbar.jsx
@@ -25,10 +25,10 @@ const Navbar = ({searchTerm, setSearchTerm, user}) => {
             <Link
             to={`user-profile/${user?._id}`} className='hidden md:block'
             >
-                <img src={user.image} alt="user" className='w-14 h-12 rounded-lg' />
+                <img src={user.image} alt="user" className='w-14 h-12 rounded-full' />
             </Link>
             <Link
-            to='create-pin' className='bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'
+            to='create-pin' className='bg-red-500 text-white rounded-full w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'
             >
                 <IoMdAdd  />
             </Link>
